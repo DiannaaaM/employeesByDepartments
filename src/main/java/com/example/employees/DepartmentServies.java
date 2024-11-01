09870bc57ad2260b5604cc67ca8d7c12cbaae58e
DiannaaaMm <murlaevadiana@gmail.com>
2024-11-01 16:01:28 +0300

    update files

--- src/main/java/com/example/employees/DepartmentServies.java
+++ src/main/java/com/example/employees/DepartmentServies.java
@@ -7,22 +7,10 @@ import java.util.List;
 import java.util.OptionalInt;
 import java.util.stream.Collectors;
 
+import static com.example.employees.EmployeeServies.employees;
+
 @Service
 public class DepartmentServies {
-    public static Employee[] employees = new Employee[10];
-
-    public DepartmentServies() {
-        employees[0] = new Employee("Иван", "Иванов", (byte) 1, 50000);
-        employees[1] = new Employee("Дмитрий", "Александров", (byte) 4, 15000);
-        employees[2] = new Employee("Евгений", "Александров", (byte) 4, 55500);
-        employees[3] = new Employee("Екатерина", "Иванова", (byte) 1, 41000);
-        employees[4] = new Employee("Ivan", "Ivanov", (byte) 5, 50000);
-        employees[5] = new Employee("Ivan", "Ivanov", (byte) 2, 50500);
-        employees[6] = new Employee("Ivan", "Ivanov", (byte) 1, 70000);
-        employees[7] = new Employee("Ivan", "Ivanov", (byte) 3, 90000);
-        employees[8] = new Employee("Ivan", "Ivanov", (byte) 2, 20000);
-        employees[9] = new Employee("Ivan", "Ivanov", (byte) 5, 10000);
-    }
 
     public static List<String> printEmployeeList(byte departanentId) {
         return Arrays.stream(employees)
